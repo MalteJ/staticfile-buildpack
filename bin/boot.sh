39a931c7243c98a2dcd465987558b3abe618fd04
Malte Janduda <mail@janduda.net>
2014-11-19 02:32:29 +0100

    making it worse 4.22

--- bin/boot.sh
+++ bin/boot.sh
@@ -33,6 +33,6 @@ touch $APP_ROOT/apache2/logs/access_log
 touch $APP_ROOT/apache2/logs/error_log
 
 (tail -f -n 0 $APP_ROOT/apache2/logs/*log &)
-exec $APP_ROOT/apache2/bin/httpd -D FOREGROUND -f $APP_ROOT/apache2/conf/httpd.conf
+exec $APP_ROOT/apache2/bin/httpd -DNO_DETACH -f $APP_ROOT/apache2/conf/httpd.conf
 
 # ------------------------------------------------------------------------------------------------
