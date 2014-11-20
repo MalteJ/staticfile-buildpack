de1e1ccbaff62f2d445b92e4c8fa755fb36a6ee7
Malte Janduda <mail@janduda.net>
2014-11-20 15:22:38 +0100

    initial commit

---
+++ Vagrantfile
@@ -0,0 +1,17 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "ubuntu/trusty64"
+
+  config.vm.synced_folder ".", "/vagrant"
+
+  #$script = <<SCRIPT
+  #cd /vagrant
+  #./bin/build_apache
+  #SCRIPT
+  #config.vm.provision :shell, :inline => $script
+end
---
+++ bin/boot.sh
@@ -0,0 +1,38 @@
+# ------------------------------------------------------------------------------------------------
+# Copyright 2013 Jordon Bedwell.
+# Apache License.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+# except  in compliance with the License. You may obtain a copy of the License at:
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the
+# License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and  limitations under the License.
+# ------------------------------------------------------------------------------------------------
+set -x
+export APP_ROOT=$HOME
+
+cd $APP_ROOT
+
+conf_file=$APP_ROOT/apache2/conf/httpd.conf
+if [ -f $APP_ROOT/htdocs/httpd.conf ]
+then
+  conf_file=$APP_ROOT/htdocs/httpd.conf
+fi
+
+mv $conf_file $APP_ROOT/apache2/orig.conf
+erb $APP_ROOT/apache2/orig.conf > $conf_file
+
+cat $conf_file
+
+# ------------------------------------------------------------------------------------------------
+
+touch $APP_ROOT/apache2/logs/access_log
+touch $APP_ROOT/apache2/logs/error_log
+
+(tail -f -n 0 $APP_ROOT/apache2/logs/*log &)
+exec $APP_ROOT/apache2/bin/httpd -DNO_DETACH -f $APP_ROOT/apache2/conf/httpd.conf
+
+# ------------------------------------------------------------------------------------------------
