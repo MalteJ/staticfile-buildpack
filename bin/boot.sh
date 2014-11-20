e59ea0972d5cdc21c9ea52746638cc1932ffa516
Malte Janduda <mail@janduda.net>
2014-11-20 22:22:20 +0100

    adding apache binary

--- bin/boot.sh
+++ bin/boot.sh
@@ -11,7 +11,7 @@
 # either express or implied. See the License for the specific language governing permissions
 # and  limitations under the License.
 # ------------------------------------------------------------------------------------------------
-
+set -x
 export APP_ROOT=/app
 
 if [ -f $APP_ROOT/apache2/htdocs/httpd.conf ]
