2af1bf8f2ee3547961a231e302f83206b636383c
Malte Janduda <mail@janduda.net>
2014-11-20 22:44:47 +0100

    build apache

--- bin/boot.sh
+++ bin/boot.sh
@@ -1,17 +1,3 @@
-# ------------------------------------------------------------------------------------------------
-# Copyright 2013 Jordon Bedwell.
-# Apache License.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
-# except  in compliance with the License. You may obtain a copy of the License at:
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software distributed under the
-# License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-# either express or implied. See the License for the specific language governing permissions
-# and  limitations under the License.
-# ------------------------------------------------------------------------------------------------
-set -x
 export APP_ROOT=/app
 
 if [ -f $APP_ROOT/apache2/htdocs/httpd.conf ]
@@ -19,24 +5,8 @@ then
   mv $APP_ROOT/apache2/htdocs/httpd.conf $APP_ROOT/apache2/conf/httpd.conf
 fi
 
-#mv $conf_file $APP_ROOT/apache2/orig.conf
-#erb $APP_ROOT/apache2/orig.conf > $conf_file
-
-#cat $conf_file
-
-# ------------------------------------------------------------------------------------------------
-
 touch $APP_ROOT/apache2/logs/access_log
 touch $APP_ROOT/apache2/logs/error_log
-chmod 777 $APP_ROOT/apache2/logs/*
 
 (tail -f -n 0 $APP_ROOT/apache2/logs/*log &)
-echo "STARTING APACHE..."
-whoami
-sleep 1
-$APP_ROOT/apache2/bin/httpd -DFOREGROUND -f $APP_ROOT/apache2/conf/httpd.conf
-echo $?
-echo "APACHE STOPPED!!!"
-sleep 10
-
-# ------------------------------------------------------------------------------------------------
+exec $APP_ROOT/apache2/bin/httpd -DFOREGROUND -f $APP_ROOT/apache2/conf/httpd.conf
