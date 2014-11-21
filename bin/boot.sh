b8e0cb05e7c8914fc724e395988bf71e8da82028
Malte Janduda <mail@janduda.net>
2014-11-21 16:09:47 +0100

    ldd --version

--- bin/boot.sh
+++ bin/boot.sh
@@ -9,4 +9,5 @@ touch $APP_ROOT/apache2/logs/access_log
 touch $APP_ROOT/apache2/logs/error_log
 
 (tail -f -n 0 $APP_ROOT/apache2/logs/*log &)
+ldd --version
 exec $APP_ROOT/apache2/bin/httpd -DFOREGROUND -f $APP_ROOT/apache2/conf/httpd.conf
