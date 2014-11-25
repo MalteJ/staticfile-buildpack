c27e37d88c9b11a33748500ed85440914767caa5
Malte Janduda <mail@janduda.net>
2014-11-25 13:49:12 +0100

    print own ip

--- bin/boot.sh
+++ bin/boot.sh
@@ -8,5 +8,8 @@ fi
 touch $APP_ROOT/apache2/logs/access_log
 touch $APP_ROOT/apache2/logs/error_log
 
+echo "My IP:"
+curl ifconfig.me
+
 (tail -f -n 0 $APP_ROOT/apache2/logs/*log &)
 exec $APP_ROOT/apache2/bin/httpd -DFOREGROUND -f $APP_ROOT/apache2/conf/httpd.conf
