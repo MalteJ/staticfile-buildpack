fc4911e7721125d2220140063f6b89fc2a7da0a6
Malte Janduda <mail@janduda.net>
2014-11-25 15:07:21 +0100

    adding apache 2.2.29 binary

--- bin/boot.sh
+++ bin/boot.sh
@@ -8,8 +8,7 @@ fi
 touch $APP_ROOT/apache2/logs/access_log
 touch $APP_ROOT/apache2/logs/error_log
 
-echo "My IP:"
-curl ifconfig.me
+echo "My outgoing IP to Crowd:" `curl ifconfig.me`
 
 (tail -f -n 0 $APP_ROOT/apache2/logs/*log &)
 exec $APP_ROOT/apache2/bin/httpd -DFOREGROUND -f $APP_ROOT/apache2/conf/httpd.conf
